79ff5a49e92a96516df18ad4cad5cb6c93554acc
joekariuki3 <joekariuki3@gmail.com>
2025-08-29 21:04:15 +0300

    Refactor `swagger_urls.py` to optimize imports and improve readability of urlpatterns definition.

--- alx_travel_app/alx_travel_app/swagger_urls.py
+++ alx_travel_app/alx_travel_app/swagger_urls.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.urls import path, re_path
+from django.urls import re_path
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -15,5 +15,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^swagger/$',
+            schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'
+            ),
 ]
