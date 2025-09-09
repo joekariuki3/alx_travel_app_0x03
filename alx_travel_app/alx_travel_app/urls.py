cdf28f332498f582917063284fe1d33bff93d79e
joekariuki3 <joekariuki3@gmail.com>
2025-09-09 22:17:12 +0300

    Add JWT authentication endpoints (`TokenObtainPairView`, `TokenRefreshView`, `TokenVerifyView`) to URLs

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -18,8 +18,17 @@ from django.contrib import admin
 from django.urls import path, include
 from . import swagger_urls
 from listings import urls as listings_urls
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(swagger_urls.urlpatterns)),
     path('api/', include(listings_urls.urlpatterns)),
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
 ]
