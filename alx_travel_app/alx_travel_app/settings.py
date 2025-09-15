d7a86d1ed2cf71f6987829e3038803f869eba106
joekariuki3 <joekariuki3@gmail.com>
2025-09-15 23:12:14 +0300

    Refactor settings.py for consistency in string formatting and improve readability

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import environ
 import os
-import dj_database_url
 from datetime import timedelta
 
 env = environ.Env()
@@ -10,90 +9,90 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
-env.read_env(os.path.join(BASE_DIR, '.env'))
+env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # ENVIRONMENT either development, testing or production
-ENVIRONMENT = env('ENVIRONMENT', default='development').upper()
+ENVIRONMENT = env("ENVIRONMENT", default="development").upper()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env(f'{ENVIRONMENT}_SECRET_KEY', default=os.urandom(256).hex())
+SECRET_KEY = env(f"{ENVIRONMENT}_SECRET_KEY", default=os.urandom(256).hex())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool(f'{ENVIRONMENT}_DEBUG', default=False)
+DEBUG = env.bool(f"{ENVIRONMENT}_DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list(f'{ENVIRONMENT}_ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = env.list(f"{ENVIRONMENT}_ALLOWED_HOSTS", default=[])
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'listings.apps.ListingsConfig',
-    'rest_framework',
-    'drf_yasg',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "listings.apps.ListingsConfig",
+    "rest_framework",
+    "drf_yasg",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     "corsheaders",
-    'django_celery_results',
-    'rest_framework_simplejwt.token_blacklist'
+    "django_celery_results",
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-CORS_ALLOW_ALL_ORIGINS=True
-ROOT_URLCONF = 'alx_travel_app.urls'
+CORS_ALLOW_ALL_ORIGINS = True
+ROOT_URLCONF = "alx_travel_app.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'alx_travel_app.wsgi.application'
+WSGI_APPLICATION = "alx_travel_app.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-db_name = env(f'{ENVIRONMENT}_DATABASE_NAME')
-db_user = env(f'{ENVIRONMENT}_DATABASE_USER')
-db_password = env(f'{ENVIRONMENT}_DATABASE_PASSWORD')
-db_host = env(f'{ENVIRONMENT}_DATABASE_HOST')
-db_port = env(f'{ENVIRONMENT}_DATABASE_PORT')
+db_name = env(f"{ENVIRONMENT}_DATABASE_NAME")
+db_user = env(f"{ENVIRONMENT}_DATABASE_USER")
+db_password = env(f"{ENVIRONMENT}_DATABASE_PASSWORD")
+db_host = env(f"{ENVIRONMENT}_DATABASE_HOST")
+db_port = env(f"{ENVIRONMENT}_DATABASE_PORT")
 
 DEVELOPMENT_DATABASE = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': db_name,
-        'HOST': db_host,
-        'USER': db_user,
-        'PASSWORD': db_password,
-        'PORT': db_port,
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": db_name,
+        "HOST": db_host,
+        "USER": db_user,
+        "PASSWORD": db_password,
+        "PORT": db_port,
     },
 }
 # production db details
@@ -104,14 +103,14 @@ DEVELOPMENT_DATABASE = {
 #     )
 # }
 PRODUCTION_DATABASE = {
-    'default': {
+    "default": {
         # 'ENGINE': 'django.db.backends.postgresql',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': db_name,
-        'HOST': db_host,
-        'USER': db_user,
-        'PASSWORD': db_password,
-        'PORT': db_port,
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": db_name,
+        "HOST": db_host,
+        "USER": db_user,
+        "PASSWORD": db_password,
+        "PORT": db_port,
     },
 }
 
@@ -121,23 +120,29 @@ TESTING_DATABASE = {
     }
 }
 
-DATABASES = DEVELOPMENT_DATABASE if ENVIRONMENT == 'DEVELOPMENT' else PRODUCTION_DATABASE if ENVIRONMENT == 'PRODUCTION' else TESTING_DATABASE
+DATABASES = (
+    DEVELOPMENT_DATABASE
+    if ENVIRONMENT == "DEVELOPMENT"
+    else PRODUCTION_DATABASE
+    if ENVIRONMENT == "PRODUCTION"
+    else TESTING_DATABASE
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -145,9 +150,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -157,44 +162,44 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-if not DEBUG and ENVIRONMENT == 'PRODUCTION':
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = "static/"
+if not DEBUG and ENVIRONMENT == "PRODUCTION":
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = 'listings.User'
+AUTH_USER_MODEL = "listings.User"
 
 # celery settings
-rb_user = env(f'{ENVIRONMENT}_RABBITMQ_USERNAME', default='guest')
-rb_password = env(f'{ENVIRONMENT}_RABBITMQ_PASSWORD', default='guest')
-rb_host = env(f'{ENVIRONMENT}_RABBITMQ_HOST', default='localhost')
-rb_port = env(f'{ENVIRONMENT}_RABBITMQ_PORT', default='5672')
+rb_user = env(f"{ENVIRONMENT}_RABBITMQ_USERNAME", default="guest")
+rb_password = env(f"{ENVIRONMENT}_RABBITMQ_PASSWORD", default="guest")
+rb_host = env(f"{ENVIRONMENT}_RABBITMQ_HOST", default="localhost")
+rb_port = env(f"{ENVIRONMENT}_RABBITMQ_PORT", default="5672")
 CELERY_BROKER_URL = f"amqp://{rb_user}:{rb_password}@{rb_host}:{rb_port}//"
-CELERY_RESULT_BACKEND = env(f'{ENVIRONMENT}_CELERY_RESULT_BACKEND', default='django-db')
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
-CELERY_RESULT_EXTENDED = env.bool(f'{ENVIRONMENT}_CELERY_RESULT_EXTENDED', default=True)
+CELERY_RESULT_BACKEND = env(f"{ENVIRONMENT}_CELERY_RESULT_BACKEND", default="django-db")
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = "UTC"
+CELERY_RESULT_EXTENDED = env.bool(f"{ENVIRONMENT}_CELERY_RESULT_EXTENDED", default=True)
 
 # Email settings console
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # rest authentication
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'SIGNING_KEY': SECRET_KEY,
-    'AUTH_HEADER_TYPES': ('Bearer',), # "Bearer <Token>"
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "SIGNING_KEY": SECRET_KEY,
+    "AUTH_HEADER_TYPES": ("Bearer",),  # "Bearer <Token>"
 }
